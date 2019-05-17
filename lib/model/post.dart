20aa0918dffee4d51036850c40a1545f7d1e22a9
17 <onesebun@gmail.com>
2019-05-17 23:17:23 +0800

    update friend list

---
+++ lib/model/post.dart
@@ -0,0 +1,11 @@
+class Post {
+  const Post({this.name});
+
+  final String name;
+}
+
+final List<Post> posts = [
+  Post(name: 'friend 1'),
+  Post(name: 'friend 2'),
+  Post(name: 'friend 3'),
+];
--- lib/widgets/friends_list.dart
+++ lib/widgets/friends_list.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../model/post.dart';
+
 class FriendsList extends StatelessWidget {
   const FriendsList();
+  Widget _listItemBuilber(BuildContext context, int index) {
+    return ListTile(
+      title: Text(posts[index].name),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text('Friend 1'),
-        ),
-        ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text('Friend 2'),
-        ),
-        ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text('Friend 3'),
-        ),
-      ],
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilber,
     );
   }
 }
