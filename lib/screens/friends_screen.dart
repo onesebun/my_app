71d44c5a5fb986284df2e793265110d6eb043384
17 <onesebun@gmail.com>
2019-05-20 21:21:49 +0800

    update friend screen

--- lib/screens/friends_screen.dart
+++ lib/screens/friends_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
 import '../widgets/friends_list.dart';
+import '../widgets/user_profile.dart';
 
 class FriendsScreen extends StatelessWidget {
   FriendsScreen();
@@ -13,20 +14,31 @@ class FriendsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.settings),
-          onPressed: () {},
-        ),
-        title: Text('好友'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.person_add),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.settings),
             onPressed: () {},
-          )
-        ],
-      ),
-      body: FriendsList(),
-    );
+          ),
+          title: Text('好友'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person_add),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                child: UserProfile(),
+              ),
+              Flexible(
+                child: FriendsList(),
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/widgets/user_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  UserProfile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      alignment: FractionalOffset.center,
+      color: Colors.greenAccent,
+      child: Text('test'),
+    );
+  }
+}
